30edc951759be9f90a10bb91d03662d97cf83365
Hunter Dyar <hunterdyar@gmail.com>
2011-03-11 16:46:26 -0500

    added person div wrapper files outline

---
+++ person.php
@@ -0,0 +1,11 @@
+<div id="personr">
+  <div id="photop">
+ 	 <img src="batman.jpg" />
+  </div>
+  <div id="data">
+  	<h3>Matt Giles '86</h3>
+  	<p>
+  		Extra Information Goes Here, man
+  	</p>
+  </div>
+</div>
