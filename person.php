61cb9be5c0d603d62d31950d992c1f2ccd21e4c8
Hunter Dyar <hunterdyar@gmail.com>
2011-03-11 21:18:36 -0500

    added another snippetyblippet

--- person.php
+++ person.php
@@ -9,3 +9,14 @@
   	</p>
   </div>
 </div>
+<?php
+//Why is this a PHP file? I dont know. 
+?>
+<div id="individualresult">
+  <div id="photop">
+ 	 <img src="morebatman.jpg" />
+  </div>
+  <div id="caption">
+  <p>Matt Giles '86</p>
+  </div>
+</div>
